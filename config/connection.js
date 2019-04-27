33a3a82fba2778a54a12c7af16c8cf1fdc58698a
olsonathan <olsonathan@gmail.com>
2019-04-26 22:34:28 -0500

    initial

--- config/connection.js
+++ config/connection.js
@@ -1,22 +1,31 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "root",
-  database: "sandwhich_db"
-});
+var connection;
+
+if (process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} 
+else {
+//	else deploy Locally
+  connection = mysql.createConnection({
+      port: 3306,
+      host: "localhost",
+      user: "root",
+      password: "root",
+      database: "sandwhich_db"
+  });
+}
 
 // Make connection.
 connection.connect(function(err) {
   if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
+      console.error("error connecting: " + err);
+      return;
   }
-  console.log("connected as id " + connection.threadId);
+  console.log("Connected ID: " + connection.threadId);
 });
 
-// Export connection for our ORM to use.
 module.exports = connection;
+
+
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 
+
+
 var PORT = process.env.PORT || 8080;
 
 var app = express();
